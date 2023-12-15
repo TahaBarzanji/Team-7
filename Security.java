47c8a4ba249bae3c12c3f8f183f9aad00a0a7d30
ckodx1 <149177851+ckodx1@users.noreply.github.com>
2023-12-15 14:12:55 +0300

    Update Security.java

--- Security.java
+++ Security.java
@@ -1,22 +1,20 @@
 import java.util.ArrayList;
-/*
- * This class represents a security system for a bank.
- */
+
+
+//This class represents a security system for a bank. 
 public class Security {
     private boolean alarmStatus;
     public ArrayList<String> thieves;
 
-    /*
-     * Constructor to initialize the security system.
-     */
+    
+    //Constructor to initialize the security system.
     public Security(ArrayList<String> thieves) {
         this.alarmStatus = false;
         this.thieves = thieves;
     }
 
-    /*
-     * @return Returns the current alarm status.
-     */
+
+    //@return Returns the current alarm status.
     public boolean isAlarmActive() {
         if (alarmStatus == true) {
             System.out.println("We wo we wo");
@@ -27,10 +25,9 @@ public class Security {
         return alarmStatus;
     }
 
-    /*
-     * The detection of a thief in the bank.
-     * Activates the alarm and triggers the safety procedures.
-     */
+    
+    //The detection of a thief in the bank.
+    //Activates the alarm and triggers the safety procedures.
     public void detectThief(String thiefName) {
         if (thieves.contains(thiefName)) {
             alarmStatus = true;
@@ -43,9 +40,7 @@ public class Security {
     }
     
     
-    /*
-     * Activates the safety procedures for the bank.
-     */
+    //Activates the safety procedures for the bank.
     private void activateSafetyProcedures() {
         System.out.println("Safety procedures activated. Please remain calm and follow instructions.");
     }
